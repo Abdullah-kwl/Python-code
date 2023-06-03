cc21e8054a8f95ccf3bcf471f10f2778b51900b0
Abdullah <mrabdullahkwl@gmail.com>
2023-06-03 20:40:19 +0500

    oop project (Bank) is completed

--- oop_bank.py
+++ oop_bank.py
@@ -1,8 +1,5 @@
 import random
 
-
-
-
 class Bank():
     random_num=random.randint(11111,99999)
     def __init__(self):
@@ -44,6 +41,7 @@ class Bank():
 
         self.menu()
 
+ 
     def access_account(self):
  
         given_name=input("Enter your name : ")
@@ -51,11 +49,57 @@ class Bank():
 
         if given_name==self.name and given_account_no==self.account_number:
             print("% ACCOUNT ACCESS GRANTED %")
+            self.access_menu()        
         else:
             print("INVALID CRIDANTIALS ! ")
             self.menu()
 
 
+    def access_menu(self):
+        print("""
+        Press Button => 
+        1 : Withdraw Money
+        2 : Deposit Money
+        3 : Check balance
+        4 : Exit
+        """)
+        input_2=int(input("Enter => "))
+        if input_2==1:
+            self.withdraw()
+        elif input_2==2:
+            self.deposit()
+        elif input_2==3:
+            self.check_balace()
+        elif input_2==4:
+            exit()
+        else:
+            print("invalid Input !")
+            self.access_menu()
 
 
-user=Bank()
+    def withdraw(self):
+        amount_withdraw= int(input("Enter Withdraw amount : "))
+        if amount_withdraw<self.balance:
+            self.balance = self.balance - amount_withdraw
+            print("% Withdraw Completed %")
+            self.access_menu()
+        else:
+            print("Invalid Balance !")
+            self.access_menu()
+
+
+    def deposit(self):
+        amount_deposit = int(input("Enter deposit amount : "))
+        self.balance = self.balance + amount_deposit
+        print("% Deposit Completed %")
+        self.access_menu()
+
+
+    def check_balace(self):
+        print("Your balance is",self.balance)
+        self.access_menu()
+
+
+
+if __name__=='__main__':
+    user=Bank()
